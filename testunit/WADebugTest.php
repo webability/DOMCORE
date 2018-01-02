be564e270d07b5f616ab0600972a7e80a4e416f1
metalwolf <phil@thomassigny.org>
2018-01-02 13:09:02 -0600

    patch 3.0.8

--- testunit/WADebugTest.php
+++ testunit/WADebugTest.php
@@ -2,28 +2,16 @@
 
 namespace testunit;
 
-if (PHP_VERSION_ID >= 70000) // travis PHP unit uses >=3.5 on PHP7+
+use PHPUnit\Framework\TestCase;
+
+class WADebugTest extends TestCase
 {
-  class WADebugTest extends \PHPUnit\Framework\TestCase
+  public function testCreate()
   {
-    public function testCreate()
-    {
-      $x = new \core\WADebug();
-      var_dump($x);
-      $this->assertEquals('OK', 'OK');
-    }
+    $x = new \core\WADebug();
+    var_dump($x);
+    $this->assertEquals('OK', 'OK');
   }
 }
-else
-{
-  class WADebugTest extends \PHPUnit_Framework_TestCase
-  {
-    public function testCreate()
-    {
-      $x = new \core\WADebug();
-      var_dump($x);
-      $this->assertEquals('OK', 'OK');
-    }
-  }
-}
+
 ?>
